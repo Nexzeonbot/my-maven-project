41340b4c39202aa4738872790039ef094865563c
Abin <abinyadhav@gmail.com>
2026-03-31 15:24:41 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,9 @@
-public class App {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello Abin! Maven project working successfully 🚀");
+        SpringApplication.run(Application.class, args);
     }
 }
